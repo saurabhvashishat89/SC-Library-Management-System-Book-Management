db1e0193aa43617b21ad18b31cd70ed4519f2ec8
2023tm93654 <2023tm93654@wilp.bits-pilani.ac.in>
2024-11-17 23:20:47 +0530

    changes for the book management

---
+++ src/main/java/com/bookexchange/management/BookManagement.java
@@ -0,0 +1,14 @@
+package com.bookexchange.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagement {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookManagement.class, args);
+	}
+
+}
